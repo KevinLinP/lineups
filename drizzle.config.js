61f5c0e3c230000927de2bd518997f0693132e2f
Kevin Lin <kevin.lin.p@gmail.com>
2025-02-16 15:21:28 -0800

    init schema

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
-	schema: './src/lib/server/db/schema.js',
-
-	dbCredentials: {
-		url: process.env.DATABASE_URL,
-		authToken: process.env.DATABASE_AUTH_TOKEN
-	},
-
-	verbose: true,
-	strict: true,
-	dialect: 'turso'
+  schema: './src/lib/server/db/schema.js',
+  verbose: true,
+  strict: true,
+  dialect: 'sqlite',
+  driver: 'd1-http',
+  dbCredentials: {
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+    token: process.env.CLOUDFLARE_D1_TOKEN,
+  },
 });
