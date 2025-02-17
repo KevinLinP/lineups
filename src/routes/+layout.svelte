9b9ad45c589e78eab6e43ac077e11145e4e4056e
Kevin Lin <kevin.lin.p@gmail.com>
2025-02-16 21:54:59 -0800

    fix darkmode?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
-
-	export const ssr = false
 </script>
 
 {@render children()}
--- tailwind.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  darkMode: 'selector',
-  // ... rest of your config
-} 
